f57574182b050ccb530823f33788f47efbe7d2f6
Christopher <cjdibella@bigpond.com>
2013-10-30 09:57:48 +1100

    let's try this on linux :)

--- som.cpp
+++ som.cpp
@@ -1,5 +1,6 @@
 #include "som.h"
 #include <cstdlib>
+#include <fstream>
 
 int main()
 {
@@ -9,17 +10,27 @@ int main()
     const type lr = 0.1;
     const type nbdw = 2.0;
     
-    kohonen<type, 2, 4, 2> run1(std::cin, lr_decay, nbdw_decay, lr, nbdw);
-    //kohonen<type, 2, 4, 4> run2(std::cin, lr_decay, nbdw_decay, lr, nbdw);
-    /*kohonen<type, 2, 2, 3> run3(std::cin, lr_decay, nbdw_decay, lr, nbdw);
-    kohonen<type, 2, 4, 2> run4(std::cin, lr_decay, nbdw_decay, lr, nbdw);
-    kohonen<type, 2, 1, 1> run5(std::cin, lr_decay, nbdw_decay, lr, nbdw);*/
+    std::ifstream file("data24.txt", std::ios::in);
+    kohonen<type, 2, 4, 2> run1(file, lr_decay, nbdw_decay, lr, nbdw);
+    file.close();
+    file.open("data24.txt", std::ios::in);
+    kohonen<type, 2, 4, 4> run2(file, lr_decay, nbdw_decay, lr, nbdw);
+    file.close();
+    file.open("data24.txt", std::ios::in);
+    kohonen<type, 2, 2, 3> run3(file, lr_decay, nbdw_decay, lr, nbdw);
+    file.close();
+    file.open("data33.txt", std::ios::in);
+    kohonen<type, 2, 4, 2> run4(file, lr_decay, nbdw_decay, lr, nbdw);
+    file.close();
+    file.open("data33.txt", std::ios::in);
+    kohonen<type, 2, 1, 4> run5(file, lr_decay, nbdw_decay, lr, nbdw);
+    file.close();
     
     run1.train(5000);
-    //run2.train(5000);
-    /*run3.train(5000);
+    run2.train(5000);
+    run3.train(5000);
     run4.train(5000);
-    run5.train(5000);*/
+    run5.train(5000);
     
     /*std::cout << run1 << std::endl;
     std::cout << run2 << std::endl;
@@ -28,10 +39,14 @@ int main()
     std::cout << run5 << std::endl;*/
     
     run1.print();
-    /*run2.print();
+    std::cout << std::endl;
+    run2.print();
+    std::cout << std::endl;
     run3.print();
+    std::cout << std::endl;
     run4.print();
-    run5.print();*/
+    std::cout << std::endl;
+    run5.print();
     
     return EXIT_SUCCESS; // Even though this is C++, I still like to do this :)
 }
